8b0d456ec89f29b7c7c56c835779fc93832690aa
Zaid Albirawi <zalbiraw@gmail.com>
2025-04-12 08:56:30 -0400

    finished k3d

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
 locals {
   password = "topsecretpassword"
 }
+
+# Install Redis Cluster when API Management is enabled
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "7.8.23"
+  namespace        = "argocd"
+  create_namespace = true
+
+  set {
+    name = "controller.readinessProbe.initialDelaySeconds"
+    value = "30"
+  }
+
+  set {
+    name  = "configs.secret.argocdServerAdminPassword"
+    value = "$2a$10$Pgp0gJC3saX42x0b7JQOB./5UVe4FXvN/5j0dXL4J76lTgiwg9NK2" # topsecretpassword
+  }
+
+  set {
+    name = "crds.keep"
+    value = "false"
+  }
+}
